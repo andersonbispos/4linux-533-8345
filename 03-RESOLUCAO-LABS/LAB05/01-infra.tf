b03269856c2ca047cf0f3330a7611c69b65e59e5
Anderson Bispo <anderson.bispo@4linux.com.br>
2023-09-12 20:46:13 -0300

    lab05

--- 03-RESOLUCAO-LABS/LAB05/01-infra.tf
+++ 03-RESOLUCAO-LABS/LAB05/01-infra.tf
@@ -20,3 +20,17 @@ resource "aws_route_table_association" "rt_public_subnet_association" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = module.vpc.rt_public_id
 }
+
+module "web" {
+  source = "./modules/instance"
+
+  instance_name = "web"
+
+  instance_zone = aws_subnet.public_subnet.availability_zone
+  instance_subnet = aws_subnet.public_subnet.id
+  instance_sgs = [module.vpc.sg_id]
+}
+
+output "web_ip" {
+    value = module.web.instance_public_ip  
+}
